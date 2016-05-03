7242e02745058a3608096c0fa32a4b8002c3c5ac
Evin1- <edwin.hdz.12.0@gmail.com>
2016-05-02 23:49:32 -0400

    Use flavors in MainActivity

--- app/src/main/java/com/example/simplepublish/MainActivity.java
+++ app/src/main/java/com/example/simplepublish/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.simplepublish;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivityTAG_";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate: " + BuildConfig.APPLICATION_ID);
+        Log.d(TAG, "onCreate: " + BuildConfig.FLAVOR);
+        Log.d(TAG, "onCreate: " + BuildConfig.TITLE_FLAVOR);
+        Log.d(TAG, "onCreate: " + getString(R.string.string_flavor));
+
     }
 }
